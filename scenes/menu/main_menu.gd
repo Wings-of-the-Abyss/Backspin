347b01c6c663294184ab0b4b8a00caefdea0fc4b
StepTheGlow <123469558+StepTheGlow@users.noreply.github.com>
2025-01-13 12:27:55 +0600

    Hover sound added

---
+++ scenes/menu/main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _process(_delta):
+	pass
+
+
+func _on_buttons_mouse_entered() -> void:
+	$hover.play()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit() # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu/settings.tscn") # Replace with function body.
